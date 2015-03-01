81984f4e42287455ea855a54e65eb24f41269fd6
Andi <admin@andreas-scharf.at>
2015-03-01 23:34:02 +0100

    removed view holder

--- samples/sample_binding/src/at/int32/android/utils/samples/binding/ViewHolder.java
+++ samples/sample_binding/src/at/int32/android/utils/samples/binding/ViewHolder.java
@@ -1,13 +1,25 @@
 package at.int32.android.utils.samples.binding;
 
 import android.app.Activity;
+import android.content.Context;
 import android.widget.Button;
 import android.widget.TextView;
-import at.int32.android.utils.ui.binding.IViewHolder;
 import at.int32.android.utils.ui.binding.controls.BindableEditText;
 import at.int32.android.utils.ui.binding.controls.BindableNumberPicker;
 
-public class ViewHolder implements IViewHolder {
+public class ViewHolder extends at.int32.android.utils.ui.binding.ViewHolder {
+
+	public ViewHolder(Context context) {
+		super(context);
+		
+		name = (TextView) context.findViewById(R.id.name);
+		age = (TextView) context.findViewById(R.id.age);
+
+		name_edit = (BindableEditText) activity.findViewById(R.id.name_edit);
+		age_edit = (BindableNumberPicker) activity.findViewById(R.id.age_edit);
+
+		reset = (Button) activity.findViewById(R.id.btn_reset);
+	}
 
 	public TextView name, age;
 
@@ -18,13 +30,4 @@ public class ViewHolder implements IViewHolder {
 	// reset button
 	public Button reset;
 
-	public ViewHolder(Activity activity) {
-		name = (TextView) activity.findViewById(R.id.name);
-		age = (TextView) activity.findViewById(R.id.age);
-
-		name_edit = (BindableEditText) activity.findViewById(R.id.name_edit);
-		age_edit = (BindableNumberPicker) activity.findViewById(R.id.age_edit);
-
-		reset = (Button) activity.findViewById(R.id.btn_reset);
-	}
 }
